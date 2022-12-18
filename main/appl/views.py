570b45450121aa6b9151063a5f03dc384a1bc872
Oleg <oleg.krut.64@gmail.com>
2022-12-18 21:32:55 +0700

    ПОИСК

--- main/appl/views.py
+++ main/appl/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from .models import Product
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, filters, generics
 from .serializers import ProductSerializer, UserSerializer
 
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductViewSet(viewsets.ModelViewSet, generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'short_description', 'description']
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet, generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'email']
